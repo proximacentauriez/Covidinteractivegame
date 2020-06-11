b9282c5ac7176097f0a2ce377547f4f2e9458e94
Proxima <proximacentauriez@gmail.com>
2020-06-11 21:28:32 +0530

    google test AD banner using test APP id(more study required)

--- CovidInteractive/app/src/main/java/com/example/covidinteractive/MainActivity.java
+++ CovidInteractive/app/src/main/java/com/example/covidinteractive/MainActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +27,21 @@ public class MainActivity extends AppCompatActivity {
     public static List<QnA_struct> QuizQuestionsMedium;
     public static List<QnA_struct> QuizQuestionsDifficult;
 
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
         if(!preLoaded)
             loadQuizQuestions();
     }
